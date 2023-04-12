2c61e518b974fb0a9f9d36fd6938d0800271008a
Aryan Tapkire <aryan100282@gmail.com>
2023-04-12 23:02:14 +0530

    Master commit

---
+++ app/src/main/java/com/example/howdyweather/Activities/ARScreen.kt
@@ -0,0 +1,12 @@
+package com.example.howdyweather.Activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.howdyweather.R
+
+class ARScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_arscreen)
+    }
+}
